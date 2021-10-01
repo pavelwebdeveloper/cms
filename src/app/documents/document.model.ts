22921507ae33a387a44cfb542bae27a563eb7c99
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-10-01 12:52:04 +0300

    added document and message components through CLI

---
+++ src/app/documents/document.model.ts
@@ -0,0 +1,3 @@
+export class Document {
+    constructor(public id: string, public name: string, public description: string, public imageUrl: string, public children: []){}
+}
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { LEADING_TRIVIA_CHARS } from '@angular/compiler/src/render3/view/template';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']*/
 })
 export class HeaderComponent implements OnInit {
+  @Output() selectedFeatureEvent = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelected(selectedEvent: string){
+    this.selectedFeatureEvent.emit(selectedEvent);
   }
 
 }
