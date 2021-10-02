c55b4c5c9ba04554ef0bbaa584788ecb568f298a
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-10-02 12:54:52 +0300

    added the logic to add messages to the messages list through message-edit component, to clear input fields

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject', {static: true}) subjectRef: ElementRef;
+  @ViewChild('msgText', {static: true}) msgTextRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Pavel';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSendMessage(event){
+    event.preventDefault();
+    const messageSubject = this.subjectRef.nativeElement.value;
+    const messageText = this.msgTextRef.nativeElement.value;
+    const newMessage = new Message('5', messageSubject, messageText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear(){
+    this.subjectRef.nativeElement.value = '';
+    this.msgTextRef.nativeElement.value = '';
   }
 
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  //messages: Array<Message> = [
+    messages: Message[] = [
+    new Message('1', 'Genealogy', 'here is a link to the list of genealogy databases: https://en.wikipedia.org/wiki/List_of_genealogy_databases', 'Ivan'),
+    new Message('2', 'Genealogy', 'I plan to visit the archive at Astrakhan', 'Oleg'),
+    new Message('3', 'Travel', 'How long does it take to travel to Kherson by car?', 'Sergei'),
+    new Message('4', 'travel', 'It takes about 5-6 hours', 'Oleg')
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message){
+    this.messages.push(message);
   }
 
 }
