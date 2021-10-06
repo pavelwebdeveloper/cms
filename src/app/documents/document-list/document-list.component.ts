fdf43d77e0f85c1d5cb2faadf10d0a3591ca5b4d
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-10-06 10:34:55 +0300

    added the logic to display documents in the document list

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Array<Document> = [
+    new Document('d1', 'Tasks list', 'This is a list for the tasks', '../images/image1.jpg', null),
+    new Document('d2', 'Instructions', 'Instructions for the tasks', '../images/image2.jpg', null),
+    new Document('d3', 'Plan', 'The plan for carrying out the tasks', '../images/image3.jpg', null),
+    new Document('d4', 'Overview', 'The overview for the topic of the tasks', '../images/image4.jpg', null)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
