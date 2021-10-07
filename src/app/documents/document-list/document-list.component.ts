914b98c5c44ad159311c27c4fb0f3a8e7d86cc2a
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-10-07 11:19:27 +0300

    added the logic to see details for each document in the documents list

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -9,10 +9,11 @@ import { Document } from '../document.model';
 export class DocumentListComponent implements OnInit {
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
   documents: Array<Document> = [
-    new Document('d1', 'Tasks list', 'This is a list for the tasks', '../images/image1.jpg', null),
-    new Document('d2', 'Instructions', 'Instructions for the tasks', '../images/image2.jpg', null),
-    new Document('d3', 'Plan', 'The plan for carrying out the tasks', '../images/image3.jpg', null),
-    new Document('d4', 'Overview', 'The overview for the topic of the tasks', '../images/image4.jpg', null)
+    new Document('d1', 'Tasks list', 'This is a list for the tasks', 'https://content.byui.edu/file/b7c3e5ed-6947-4971-9d32-4e5b6b397cac/CIT 366 course tasks list.pdf', null),
+    new Document('d2', 'Instructions', 'Instructions for the tasks', 'https://content.byui.edu/file/b7c3e5ed-6947-4971-9d32-4e5b6b397cac/CIT 366 course instructions.pdf', null),
+    new Document('d3', 'Plan', 'The plan for carrying out the tasks', 'https://content.byui.edu/file/b7c3e5ed-6947-4971-9d32-4e5b6b397cac/CIT 366 course plan.pdf', null),
+    new Document('d4', 'Overview', 'The overview for the topic of the tasks', 'https://content.byui.edu/file/b7c3e5ed-6947-4971-9d32-4e5b6b397cac/CIT 366 course overview.pdf', null),
+    new Document('d5', 'Schedule', 'The schedule for carrying out the tasks', 'https://content.byui.edu/file/b7c3e5ed-6947-4971-9d32-4e5b6b397cac/CIT 366 course schedule.pdf', null)
   ];
 
   constructor() { }
