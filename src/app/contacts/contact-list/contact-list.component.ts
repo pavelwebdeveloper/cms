8614d734a27d8ce4d98fbb824c93a498539091cf
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-10-23 17:12:43 +0300

    defined [routerLink] Directives to add a new contact, to load the contact detail component, for the edit button and implemented the contact delete button

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -8,23 +8,20 @@ import { ContactService } from '../contact/contact.service';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  //@Output() selectedContactEvent = new EventEmitter<Contact>();
-  //contacts: Contact[] = [
-    /*contacts: Array<Contact> = [
-    new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg", null),
-    new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/barzeer.jpg", null)
-  ];*/
+
   contacts: Contact[] = [];
 
   constructor(private contactService: ContactService) { }
 
   ngOnInit(): void {
     this.contacts = this.contactService.getContacts();
+    this.contactService.contactChangedEvent.subscribe(
+      (contacts: Contact[]) => {
+        this.contacts = contacts;
+      }
+    )
   }
 
-  onSelected(contact: Contact){
-    //this.selectedContactEvent.emit(contact);
-    this.contactService.contactSelectedEvent.emit(contact);
-  }
+  
 
 }
