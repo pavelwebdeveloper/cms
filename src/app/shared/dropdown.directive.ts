3c001828721e807243f6be669811693f312edf06
Pavel Shvets <Pavel.v.shvets@gmail.com>
2025-12-06 11:44:13 -0700

    moved menu in the navbar to the right. Fixed overflow. Added comments. Modified links in the index.html file for bootstrap, jquery. Added link for popper

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,40 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, HostBinding, HostListener, ElementRef } from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
-    @HostBinding('class.open') isOpen = false;
+    @HostBinding('class.show') isOpen = false;
+
+    /*  */
+    constructor(private elRef: ElementRef) {}
 
     @HostListener('click') toggleOpen(){
         this.isOpen = !this.isOpen;
+
+        const menu = this.elRef.nativeElement.querySelector('.dropdown-menu');
+        if (menu) {
+          if (this.isOpen) {
+            menu.classList.add('show');
+          } else {
+            menu.classList.remove('show');
+          }
+        }
+
     }
+
+
+    // Close dropdown when clicking outside of it.
+    @HostListener('document:click', ['$event'])
+    closeWhenClickedOutside(event: Event) {
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+
+      const menu = this.elRef.nativeElement.querySelector('.dropdown-menu');
+      if (menu) {
+        menu.classList.remove('show');
+      }
+    }
+  }
 
 }
